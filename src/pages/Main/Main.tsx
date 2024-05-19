3267ea9f8e07f22c5778c260a1dd54c64c967e66
Anton N <at0xa32@gmail.com>
2024-05-19 23:25:28 +0300

    fix: small fixes

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,10 @@
 import { memo } from 'react';
-import { classNames } from '@/shared/classNames/classNames';
 import cls from './Main.module.scss';
 import { Header } from './ui/Header/Header';
 import { Content } from './ui/Content/Content';
 
 const Main = memo(() => (
-    <div className={classNames(cls.Main, {}, [])}>
+    <div className={cls.Main}>
         <div className={cls.headerWrapper}>
             <Header />
         </div>
--- src/pages/Main/ui/Header/Header.tsx
+++ src/pages/Main/ui/Header/Header.tsx
@@ -1,31 +1,32 @@
 import { memo, useCallback, useState } from 'react';
 import { useTypedDispatch } from '@/hooks/useTypedDispatch';
-import { gameActions } from '@/store/reducers/game';
 
+import { useTypedSelector } from '@/hooks/useTypedSelector';
+import {
+    getGameType, getSearch, setGameType, setSearch,
+} from '@/store/reducers/game/services/action-creators';
 import { GameType } from '@/store/reducers/game/types';
 import { HeaderView } from './ui/HeaderView';
-import { useTypedSelector } from '@/hooks/useTypedSelector';
-import { getGameType, getSearch } from '@/store/reducers/game/services/action-creators';
 
 export const Header = memo(() => {
     const dispatch = useTypedDispatch();
     const gameType = useTypedSelector(getGameType);
     const gameSearch = useTypedSelector(getSearch);
-    const [search, setSearch] = useState<string>(gameSearch);
+    const [searchValue, setSearchValue] = useState<string>(gameSearch);
 
     const handleChangeGameType = useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
-        dispatch(gameActions.setGameType(e.target.value as GameType));
-        dispatch(gameActions.setSearch(''));
-        setSearch('');
+        dispatch(setGameType(e.target.value as GameType));
+        dispatch(setSearch(''));
+        setSearchValue('');
     }, [dispatch]);
 
     const handleChangeInput = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
+        setSearchValue(e.target.value);
     }, []);
 
     const handleSearchButton = useCallback(() => {
-        dispatch(gameActions.setSearch(search));
-    }, [dispatch, search]);
+        dispatch(setSearch(searchValue));
+    }, [dispatch, searchValue]);
 
     const handleEnterPress = useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
@@ -40,7 +41,7 @@ export const Header = memo(() => {
             handleChangeInput={handleChangeInput}
             handleSearchButton={handleSearchButton}
             gameType={gameType}
-            search={search}
+            search={searchValue}
         />
     );
 });
